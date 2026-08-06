dc0850072c7db0786008016b2f1af291613c9409
prnshu-p <pranshupatel329@gmail.com>
2026-08-06 19:06:54 +0530

    fix(misc): complete NearestSmallerValues monotonic stack solution
    Replace incomplete stub with working implementation using a monotonic stack to find the nearest smaller value to the left for each element.

--- questions/NearestSmallerValues.cpp
+++ questions/NearestSmallerValues.cpp
@@ -2,14 +2,34 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n;
     cin >> n;
 
-    vector<int> val;
-    for (int i = 0; i < n; i++) {
-        cin >> val[i];
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
     }
 
-   
- 
+    stack<int> st;
+
+    for (int i = 1; i <= n; i++) {
+        
+        while (!st.empty() && a[st.top()] >= a[i]) {
+            st.pop(); 
+        }
+
+        if (st.empty()) {
+            cout << 0 << " "; 
+        } else {
+            cout << st.top() << " "; 
+        }
+
+        st.push(i);
+    }
+
+    cout << "\n";
+    return 0;
 }
